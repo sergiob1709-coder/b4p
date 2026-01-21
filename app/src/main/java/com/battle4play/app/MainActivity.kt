3e0aae551a658c0a24ef641ff885d7618dca7aaf
sergiob1709-coder <sergiob1709@gmail.com>
2026-01-21 16:40:48 +0100

    Add missing height import

--- app/src/main/java/com/battle4play/app/MainActivity.kt
+++ app/src/main/java/com/battle4play/app/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -163,8 +165,6 @@ fun Battle4PlayScreen() {
                             ) {
                                 Text(text = "Reintentar")
                             }
-                        }) {
-                            Text("Reintentar")
                         }
                     }
                 }
